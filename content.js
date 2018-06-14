d01e0a1a541baf4633f6e869af3ea581b7e6d42e
himaneesh <himaneeshpathak@gmail.com>
2018-06-14 22:07:51 +0530

    :contains()

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 $('#app').on("click", "[title='Menu']", function () {
-    $("ul._3s1D4").append('<li tabindex="-1" class="_10anr vidHz _28zBA" id="DownloadButton" data-animate-dropdown-item="true" style="opacity: 1; transform: translateY(0px);"><div class="_3lSL5 _2dGjP" role="button" title="Download Info">Download Contact Info</div></li>');
+	
+
     $("#DownloadButton").click(function () {
         console.log("Button Clicked")
     });
+
 });
 
